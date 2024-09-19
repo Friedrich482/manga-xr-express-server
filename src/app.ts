20e20bc7ece0d70840d92a018406a79fe6991759
Friedrich482 <friedricht109@gmail.com>
2024-09-20 01:01:03 +0200

    fix module

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { fetchChapterPages } from "./utils/fetchChapterPages";
+import fetchChapterPages from "./utils/fetchChapterPages";
 const app = express();
 const PORT = 5000;
 
